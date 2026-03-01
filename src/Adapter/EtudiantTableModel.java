ceea200c9b2550228f95525b0838b478fc73a684
Louay Rougui <louaymalem@gmail.com>
2026-03-01 15:23:46 +0100

    added deleting and Search filtering to gestion etudiant

--- src/Adapter/EtudiantTableModel.java
+++ src/Adapter/EtudiantTableModel.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class EtudiantTableModel extends AbstractTableModel {
     ResultSetMetaData rsmd;
     ArrayList<Object[]> data;
+    ArrayList<Object[]> filteredData;
     EtudiantImplementation implementation;
 
     public EtudiantTableModel(ResultSet rs, EtudiantImplementation implementation) {
@@ -28,6 +29,7 @@ public class EtudiantTableModel extends AbstractTableModel {
                 }
                 data.add(ligne);
             }
+            filteredData = new ArrayList<>(data);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -35,7 +37,7 @@ public class EtudiantTableModel extends AbstractTableModel {
 
     @Override
     public int getRowCount() {
-        return data.size();
+        return filteredData.size();
     }
 
     @Override
@@ -49,7 +51,7 @@ public class EtudiantTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return data.get(rowIndex)[columnIndex];
+        return filteredData.get(rowIndex)[columnIndex];
     }
 
     @Override
@@ -68,29 +70,75 @@ public class EtudiantTableModel extends AbstractTableModel {
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        int cin=(int) data.get(rowIndex)[0];
-        String nom=(String) data.get(rowIndex)[1];
-        String prenom=(String) data.get(rowIndex)[2];
-        Double newMoyenne = Double.parseDouble(aValue+"");
-        int a=implementation.modifyEudiant(cin,nom,prenom,newMoyenne);
-        if(a>0){
-        data.get(rowIndex)[columnIndex] = aValue;
+        int cin = (int) data.get(rowIndex)[0];
+        String nom = (String) data.get(rowIndex)[1];
+        String prenom = (String) data.get(rowIndex)[2];
+        Double newMoyenne = Double.parseDouble(aValue + "");
+        int a = implementation.modifyEudiant(cin, nom, prenom, newMoyenne);
+        if (a > 0) {
+            data.get(rowIndex)[columnIndex] = aValue;
 
-        }else {
-            JOptionPane.showInputDialog(null,"erreurs de la modification");
+        } else {
+            JOptionPane.showInputDialog(null, "erreurs de la modification");
         }
     }
 
     public void ajouterEtudiant(int cin, String nom, String prenom, double moyenne) {
         int x = implementation.insertEtudiant(cin, nom, prenom, moyenne);
-        if(x>0){
-            Object[] newLigne=new Object[4];
-            newLigne[0]=cin;
-            newLigne[1]=nom;
-            newLigne[2]=prenom;
-            newLigne[3]=moyenne;
+        if (x > 0) {
+            Object[] newLigne = new Object[4];
+            newLigne[0] = cin;
+            newLigne[1] = nom;
+            newLigne[2] = prenom;
+            newLigne[3] = moyenne;
             data.add(newLigne);
+            filteredData.add(newLigne);
             fireTableDataChanged();
+        } else {
+            System.out.println("pas de modification");
         }
+    }
+    public void supprimerEtudiant(int cin) {
+        int x = implementation.deleteEudiant(cin);
+        if (x > 0) {
+            for (int i = 0; i < data.size(); i++) {
+                if ((int) data.get(i)[0] == cin) {
+                    data.remove(i);
+                    break;
+                }
+            }
+            for (int i = 0; i < filteredData.size(); i++) {
+                if ((int) filteredData.get(i)[0] == cin) {
+                    filteredData.remove(i);
+                    break;
+                }
+            }
+            /*
+            data.removeIf(row -> (int) row[0] == cin);
+            filteredData.removeIf(row -> (int) row[0] == cin);
+            */
+            fireTableDataChanged();
+        } else {
+            System.out.println("pas de modification");
+        }
+    }
+
+    public void filterUpdate(String filter) {
+        filteredData.clear();
+        if (filter.isEmpty()) {
+            filteredData.addAll(data);
+        } else {
+
+            filter = filter.toLowerCase();
+            for (int i = 0; i < data.size(); i++) {
+                String nomPrenom = (String) data.get(i)[1] + (String) data.get(i)[2];
+                nomPrenom = nomPrenom.toLowerCase();
+                if (nomPrenom.contains(filter)) {
+                    filteredData.add(data.get(i));
+                }
+            }
+        }
+        fireTableDataChanged();
+
     }
 }
--- src/IHM/GestionEtudiant.java
+++ src/IHM/GestionEtudiant.java
@@ -2,29 +2,38 @@ package IHM;
 
 import Adapter.EtudiantTableModel;
 import DataBase.EtudiantImplementation;
+import Model.DataProfile;
+import Model.Profile;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.ResultSet;
 
 public class GestionEtudiant extends JFrame {
     JTable jt;
-    JTextField tcin,tprenom,tnom,tmoy;
+    JTextField tcin, tprenom, tnom, tmoy, tsearch;
+    JButton btn_save;
+
     EtudiantTableModel model;
-    GestionEtudiant (){
+
+    GestionEtudiant() {
         setTitle("Gestion Etudiants:");
-        setSize(900,600);
+        setSize(900, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         //requete
-        String requette="Select * from etudiant";
-        EtudiantImplementation implementation=new EtudiantImplementation();
-        ResultSet rs=implementation.selectEtudiant(requette);
-        model = new EtudiantTableModel(rs,implementation);
+        String requette = "Select * from etudiant";
+        EtudiantImplementation implementation = new EtudiantImplementation();
+        ResultSet rs = implementation.selectEtudiant(requette);
+        model = new EtudiantTableModel(rs, implementation);
 
 
-        jt=new JTable();
+        jt = new JTable();
         jt.setModel(model);
         //creation formulaire
         JPanel jp = new JPanel();
@@ -36,7 +45,7 @@ public class GestionEtudiant extends JFrame {
         tnom = new JTextField(10);
         tprenom = new JTextField(10);
         tmoy = new JTextField(10);
-        JButton btn_save = new JButton("Save");
+        btn_save = new JButton("Save");
         jp.add(lcin);
         jp.add(tcin);
         jp.add(lnom);
@@ -49,19 +58,71 @@ public class GestionEtudiant extends JFrame {
         btn_save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                model.ajouterEtudiant(Integer.parseInt(tcin.getText()),tnom.getText(),tprenom.getText(),Double.parseDouble(tmoy.getText()));
+                model.ajouterEtudiant(Integer.parseInt(tcin.getText()), tnom.getText(), tprenom.getText(), Double.parseDouble(tmoy.getText()));
             }
         });
+        //textfield Search
+        JPanel jpSouth = new JPanel();
+        tsearch = new JTextField(20);
+        //search logic
+        tsearch.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                model.filterUpdate(tsearch.getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                model.filterUpdate(tsearch.getText());
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                model.filterUpdate(tsearch.getText());
+
+            }
+        });
+        //ajout dans une label
+        JLabel lsearch = new JLabel("Search:");
+        jpSouth.add(lsearch);
+        jpSouth.add(tsearch);
+        //ajout suppression
+        jt.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    int row = jt.rowAtPoint(e.getPoint());
+                    jt.setRowSelectionInterval(row, row);
+                    JPopupMenu popUp = new JPopupMenu();
+                    JMenuItem supprimer = new JMenuItem("Supprimer");
+                    supprimer.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            int index = jt.getSelectedRow();
+                            int cin = (int) model.getValueAt(index, 0);
+                            int confirm = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete student with CIN: " + cin + "?", "Confirm Delete", JOptionPane.YES_NO_OPTION);
+                            if (confirm == JOptionPane.YES_OPTION) {
+                                model.supprimerEtudiant(cin);
+                            }
+                        }
+                    });
+                    popUp.add(supprimer);
+                    popUp.show(jt, e.getX(), e.getY());
+                }
+            }
+        });
+        //ajout dans le window
         this.add(new JScrollPane(jt), BorderLayout.CENTER);
-        this.add(jp,BorderLayout.NORTH);
+        this.add(jp, BorderLayout.NORTH);
+        this.add(jpSouth, BorderLayout.SOUTH);
 
     }
 
 
-
-
     public static void main(String[] args) {
-        GestionEtudiant ge= new GestionEtudiant();
+        GestionEtudiant ge = new GestionEtudiant();
         ge.setVisible(true);
     }
 
